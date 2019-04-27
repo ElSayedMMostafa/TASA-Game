90cf8b048705ad0d3601ea824ab3bade217eae99
elSayed Moustafa <sayedmmostafa255@gmail.com>
2019-04-27 13:49:59 +0200

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -6,19 +6,19 @@
 #include "EnemyCell.h"
 #include "ObstacleCell.h"
 #include "DateCell.h"
-Grid::Grid() 
+Grid::Grid()
 {
 	// initializes all the GameObject pointer of the List to NULL
 	for (int i = 0; i < NumRowCells; i++)
 	{
 		for (int j = 0; j < NumColumnCells; j++)		{
-			
-			GameCells[i][j] = new EmptyCell(i,j);	//start with all cells as empty cells
+
+			GameCells[i][j] = new EmptyCell(i, j);	//start with all cells as empty cells
 		}
 	}
 	pGUI = new GUI;
 	DrawAllCells();
-	
+
 }
 
 
@@ -27,19 +27,19 @@ Grid::Grid()
 // Moves the entered position in the passed direction if possible
 // updates currentCell if move takes place
 // Note: It returns bool indicating if a move occured or not
-bool Grid::MoveIfPossible(Cell* pCurrentCell, ActionType dir) 
+bool Grid::MoveIfPossible(Cell* pCurrentCell, ActionType dir)
 {
-	
+
 	//Calculate the row and col of the cell to move to
 	//First initialize them as current row and col
 	int playerOldRow = pCurrentCell->getRow();
 	int playerOldCol = pCurrentCell->getCol();
 
 	int TargetRow = playerOldRow;
-	int TargetCol = playerOldCol ; 
+	int TargetCol = playerOldCol;
 
-	
-	switch(dir)
+
+	switch (dir)
 	{
 	case MOVE_UP:
 		TargetRow--;
@@ -55,28 +55,28 @@ bool Grid::MoveIfPossible(Cell* pCurrentCell, ActionType dir)
 		break;
 	}
 
-	if( !IsValidCell(TargetRow, TargetCol))
+	if (!IsValidCell(TargetRow, TargetCol))
 		return false;
 
 	//get the target cell
 	Cell *pTargetCell = GameCells[TargetRow][TargetCol];
 
-	if(!pTargetCell->ActOn(player))	//The target cell is an obstacle ==> no action should be taken
+	if (!pTargetCell->ActOn(player))	//The target cell is an obstacle ==> no action should be taken
 		return false;
 
 	//The target cell is not an obstacle and has acted on the player
 	//The player position should be updated to the target cell
-	
+
 	//Player should be moved to the target cell
 	//1-delete the target cell and make it point to the PlayerCell
-	setCell(TargetRow,TargetCol, pCurrentCell);
+	setCell(TargetRow, TargetCol, pCurrentCell);
 
 	//2- update player position
 	pCurrentCell->SetRow(TargetRow);
 	pCurrentCell->SetCol(TargetCol);
 
 	//3- Convert the old player position to an Empty cell
-	GameCells[playerOldRow][playerOldCol] = new EmptyCell(playerOldRow,playerOldCol);
+	GameCells[playerOldRow][playerOldCol] = new EmptyCell(playerOldRow, playerOldCol);
 
 	//4- Redraw both cells
 	pGUI->DrawCell(GameCells[TargetRow][TargetCol]);
@@ -85,22 +85,22 @@ bool Grid::MoveIfPossible(Cell* pCurrentCell, ActionType dir)
 	return true;
 }
 
-void Grid::DrawAllCells() const	
+void Grid::DrawAllCells() const
 {
 	// Draw Empty Grid
 	pGUI->ClearGridArea();
 
 	// Draw all the GameObject of the Grid
 	for (int i = 0; i < NumRowCells; i++)
-		for (int j = 0; j < NumColumnCells; j++)
-			pGUI->DrawCell(GameCells[i][j]);
+	for (int j = 0; j < NumColumnCells; j++)
+		pGUI->DrawCell(GameCells[i][j]);
 }
 
 void Grid::setCell(int row, int col, Cell* pCell)
-{	
-	if(GameCells[row][col])
+{
+	if (GameCells[row][col])
 		delete GameCells[row][col];	//if cell ptr is not null, delete it first
-	
+
 	GameCells[row][col] = pCell;
 
 }
@@ -117,13 +117,13 @@ GUI * Grid::getGUI() const
 ActionType Grid::GetUserAction() const
 {
 	// Ask the input to get the action from the user.
-	return pGUI->GetUserAction( ); 
+	return pGUI->GetUserAction();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 // Creates an action and executes it
-void Grid::ExecuteAction(ActionType ActType) 
+void Grid::ExecuteAction(ActionType ActType)
 {
 	// According to Action Type, create the corresponding action object	
 	switch (ActType)
@@ -145,16 +145,13 @@ void Grid::ExecuteAction(ActionType ActType)
 		pGUI->setInterfaceMode(MODE_MENU);
 		break;
 
-	case ENEMY:
-		cout << "It's enemy dude" << endl;
-
-	///TODO: Add a case for EACH Action type
+		///TODO: Add a case for EACH Action type
 	case MOVE_UP:
 	case MOVE_DOWN:
 	case MOVE_RIGHT:
 	case MOVE_LEFT:
-		player->Move(this,ActType);
-		break;		
+		player->Move(this, ActType);
+		break;
 
 	case STATUS:	// a click on the status bar ==> no action
 		return;
@@ -175,31 +172,34 @@ Grid::~Grid()
 
 void Grid::RunApp()
 {
-	
-	int row = 7, col=12;	
-	PlayerCell* pCell = new PlayerCell(row,col);
-	setCell(row,col, pCell);
+
+	int row = 7, col = 12;
+	PlayerCell* pCell = new PlayerCell(row, col);
+	setCell(row, col, pCell);
 	player = new Player(*pCell);
 	pGUI->DrawCell(pCell);
-	
+
 	while (1)
 	{
 		ActionType act = GetUserAction();
-		if(act == EXIT)
+		if (act == EXIT)
 			return;
-		
-		ExecuteAction(act,pCell);
+
+		ExecuteAction(act, pCell);
 		if (act == ENEMY){
 			EnemyCell *enemy = new EnemyCell(desiredRow, desiredColumn);
 			pGUI->DrawCell(enemy);
+			pGUI->PrintMessage("");
 		}
 		else if (act == DATEE){
 			DateCell *datee = new DateCell(desiredRow, desiredColumn);
 			pGUI->DrawCell(datee);
+			pGUI->PrintMessage("");
 		}
 		else if (act == OBSTACLE){
 			ObstacleCell *obstacle = new ObstacleCell(desiredRow, desiredColumn);
 			pGUI->DrawCell(obstacle);
+			pGUI->PrintMessage("");
 		}
 		Sleep(100);
 	}
@@ -209,8 +209,8 @@ void Grid::RunApp()
 ////////////////////////////////////////////////////////////////////////////////////
 bool Grid::IsValidCell(int row, int col) const
 {
-	return (row >= 0 && row < NumRowCells) 
-			&& (col >=0 && col < NumColumnCells);
+	return (row >= 0 && row < NumRowCells)
+		&& (col >= 0 && col < NumColumnCells);
 }
 
 // Creates aother action and executes it [special one made by SAYED to be applicable to use it with SAVING 
@@ -235,15 +235,17 @@ void Grid::ExecuteAction(ActionType ActType, Cell* myCell)
 	case PAUSE:	//pause game
 		pGUI->setInterfaceMode(MODE_MENU);
 		break;
-	
+
 	case OBSTACLE:
 	case DATEE:
-	case ENEMY: 
+	case ENEMY:
+		//pGUI->setInterfaceMode(MODE_MENU);
+		pGUI->PrintMessage("Choose The Desired Cell");
 		pGUI->GetPointClicked(x, y);
 		desiredRow = (y - ToolBarHeight) / GridCellWidth;
 		desiredColumn = x / GridCellWidth;
 		break;
-		
+
 		///TODO: Add a case for EACH Action type
 	case MOVE_UP:
 	case MOVE_DOWN:
